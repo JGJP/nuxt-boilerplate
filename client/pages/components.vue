58d56adb36c863e548ce11dd09c9f094def28db8
J Garcia <j@jgarcia.jp>
2020-01-16 17:15:59 +0900

    tables

--- client/pages/components.vue
+++ client/pages/components.vue
@@ -2,30 +2,62 @@
 
 	#buttons
 		section
-			.container.space-items
-				h1 buttons
-				p default
-				.space-items-horz
-					button button
-					button.ghost button
-					a.button anchor
-					a.button.ghost anchor ghost
-				p secondary
-				.space-items-horz
-					button.secondary button
-					button.secondary.ghost button
-		section.inverted
-			.container.space-items
-				p inverted
-				.space-items-horz
-					button button
-					button.ghost button
+			.section
+				.container.space-items
+					h1 buttons
+					p default
+					.space-items-horz
+						button button
+						button.ghost button
+						a.button anchor
+						a.button.ghost anchor ghost
+					p secondary
+					.space-items-horz
+						button.secondary button
+						button.secondary.ghost button
+			.section.inverted
+				.container.space-items
+					p inverted
+					.space-items-horz
+						button button
+						button.ghost button
+			.section
+				.container.space-items
+					p call to action
+					.space-items-horz
+						button.cta button
+						button.cta.ghost button
+
 		section
 			.container.space-items
-				p call to action
-				.space-items-horz
-					button.cta button
-					button.cta.ghost button
+				h1 tables
+				table.borders
+					tr
+						th header
+						th header
+						th header
+					tr
+						td cell
+						td cell
+						td cell
+					tr
+						td cell
+						td cell
+						td cell
+
+				table.borders
+					tr
+						th header
+						td cell
+						td cell
+					tr
+						th header
+						td cell
+						td cell
+					tr
+						th header
+						td cell
+						td cell
 
 
 </template>
