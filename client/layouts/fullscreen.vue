32e8e1e5b446352fe787e70398f2457026bb15e4
J Garcia <j@jgarcia.jp>
2020-01-16 21:03:04 +0900

    fullscreen layout

---
+++ client/layouts/fullscreen.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+
+	#layout-fullscreen.layout
+		main
+			nuxt
+
+</template>
+
+
+
+<style lang="sss">
+
+@import "../assets/vars.sss"
+
+body
+	overflow-y: auto
+
+#layout-fullscreen
+	main
+		> *:first-child
+			min-height: 100vh
+		margin-top: 0
+		@media ($nav-breakpoint)
+			margin-top: 0
+		> *:first-child
+			@mixin big-vert-padding-bottom
+
+</style>
+
+
+
+<script>
+
+	import Header from '~/components/Header';
+	import Footer from '~/components/Footer';
+
+	export default {
+		components: {
+			Header,
+			Footer,
+			},
+		}
+
+</script>
