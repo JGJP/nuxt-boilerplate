c4ec6be3cd3ee39ab6d655dd9dbbcdaafa8bd7a9
J Garcia <j@jgarcia.jp>
2020-01-21 22:21:30 +0900

    rebuild

--- util/fish-functions/rebuild.fish
+++ util/fish-functions/rebuild.fish
@@ -1,6 +1,6 @@
 function rebuild --description 'alias build=yarn build'
-	git pull;
 	cd ~/site/client/;
+	git pull;
 	yarn build;
 	rm -rf /usr/share/nginx/html/*;
 	cp -a dist/* /usr/share/nginx/html/;
