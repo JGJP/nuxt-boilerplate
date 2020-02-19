6708500f44de8207a81042b896d61140287ee051
J Garcia <j@jgarcia.jp>
2020-02-19 14:06:22 +0900

    noindex

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -160,6 +160,7 @@ export default {
 		titleTemplate: "%s | NUXT",
 		meta: [
 			{ charset: "utf-8" },
+			{ name: "robots", content: "noindex, nofollow, noimageindex, noarchive" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
 			{ hid: "description", name: "description", content: "description content", },
 		],
