5aa928aae8fc4368e32851491575e57188a075ba
J Garcia <j@jgarcia.jp>
2020-02-21 20:10:49 +0900

    replace quotes in router

--- client/router.js
+++ client/router.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue"
+import Router from "vue-router"
 
-import top from '~/pages/index.vue'
-import components from '~/pages/components.vue'
+import top from "~/pages/index.vue"
+import components from "~/pages/components.vue"
 
 Vue.use(Router)
 
 export function createRouter() {
 	return new Router({
-		mode: 'history',
+		mode: "history",
 		routes: [
-			{ path: '/', component: top },
-			{ path: '/components/', component: components },
+			{ path: "/", component: top },
+			{ path: "/components/", component: components },
 		]
 	})
 }
