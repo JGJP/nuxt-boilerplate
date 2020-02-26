08a3f6ca350ee1b7be9982c7df7ebaf5e2f54213
J Garcia <j@jgarcia.jp>
2020-02-26 16:10:07 +0900

    error page

---
+++ client/layouts/error.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+
+	#error
+		section
+			.container.space-items.errorText
+				h2(v-if="error.statusCode === 404") 404 error
+				h2(v-else) error occured
+
+</template>
+
+
+
+<style lang="sss">
+
+@import "../assets/vars.sss"
+
+#error
+	.errorText
+		@media ($not-phone)
+			min-height: 50vh
+
+</style>
+
+
+
+<script>
+
+	export default {
+		props: ["error"],
+		layout: "default",
+		head(){
+			return {
+				title: "error",
+			}
+		},
+	}
+
+</script>
