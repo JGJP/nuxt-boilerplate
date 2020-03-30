c53b842918aa7520c6e8a7e4258481b2d7109234
J Garcia <j@jgarcia.jp>
2020-03-30 15:40:13 +0900

    form page

--- client/pages/form.vue
+++ client/pages/form.vue
@@ -6,7 +6,7 @@
 				h1 form
 			.container.narrow
 				.box
-					form.space-items-big(action="https://formspree.io/someid" method="POST" v-scroll-reveal)
+					form.lines.space-items-big(action="https://formspree.io/someid" method="POST" v-scroll-reveal)
 						input.full(type="email" name="email" placeholder="email")
 						div
 							textarea-autosize(:min-height="72" name="message" placeholder="message")
@@ -43,9 +43,11 @@
 <style lang="sss" scoped>
 
 	@import "../assets/vars.sss"
-	.box-shadow,
-	.box-shadow-big
-		height: $unit6
-		padding: $unit1
+	form
+		textarea
+			width: 100%
+			// resize: none
+			height: 2.4em
+			// max-height: 20em
 
 </style>
